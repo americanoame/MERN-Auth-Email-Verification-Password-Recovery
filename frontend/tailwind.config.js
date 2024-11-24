93186051624b63116c3f6dec8b8ffe21008c7dfc
Andre Silva <andrenations@gmail.com>
2024-11-23 23:00:42 -0500

    inicial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+export const content = [
+  "./index.html", // Include your HTML files
+  "./src/**/*.{js,jsx,ts,tsx}", // Include all JavaScript/TypeScript files in src
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
