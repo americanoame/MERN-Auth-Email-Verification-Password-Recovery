6bf6cca1a9154461cadf5b11ffd5c14c1f45add6
Andre Silva <andrenations@gmail.com>
2024-11-24 23:32:13 -0500

    Implement welcome email functionality with Mailtrap integration

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -20,3 +20,27 @@ export const sendVerificationEmail = async (email, verificationToken) => {
 		throw new Error(`Error sending verification email: ${error}`);
 	}
 };
+
+
+
+export const sendWelcomeEmail = async (email, name) =>  {
+	const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			template_uuid: "dde1fecb-02f8-44db-b844-91b8c4afb867",
+			template_variables: {
+				company_info_name: "Fantasy Corp",
+				name: name,
+			},
+		});
+
+		console.log("Welcome Email sent successfully", response);
+	} catch (error) {
+		console.error(`Error sending welcome email`, error);
+
+		throw new Error(`Error sending welcome email: ${error}`);
+	}
+}
