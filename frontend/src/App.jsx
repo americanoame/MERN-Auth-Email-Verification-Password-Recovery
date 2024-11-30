9a4d17da0efa1ee7e22872f65653cc001997d88a
Andre Silva <andrenations@gmail.com>
2024-11-29 23:53:45 -0500

    feat: Implemented basic routing for authentication pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import EmailVerification from "./pages/EmailVerification";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/email-verification" element={<EmailVerification />} />
+      </Routes>
+    
+  );
+};
 
-export default App
+export default App;
